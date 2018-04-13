1f4ea8b5071f0823a5127e7561de83cac3a9de22
Unknown <nemesismse@gmail.com>
2018-04-13 12:25:23 -0400

    Committing first week's assignment

---
+++ Assets/Playermovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playermovement : MonoBehaviour {
+
+	public KeyCode rightKey = KeyCode.RightArrow;
+	public KeyCode leftKey = KeyCode.LeftArrow;
+
+	public float moveSpeed = 1f;
+
+	// Use this for initialization
+	void Start () {
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey(rightKey)) {
+			transform.Translate (moveSpeed, 0, 0);
+		}
+		if (Input.GetKey(leftKey)) {
+			transform.Translate (-moveSpeed, 0, 0);
+		}
+
+	}
+}
